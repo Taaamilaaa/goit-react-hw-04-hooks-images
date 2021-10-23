36cdf7ed19983c7ba3ee1382d8102d879dfd2e94
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-10-23 17:19:47 +0300

    summary

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,21 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Searchbar } from "../components/Searchbar/Searchbar";
 import { ImageGallery } from "../components/ImageGallery/ImageGallery";
 import { pixabayFetch } from "../Services/pixabay";
 import { Button } from "../components/Button/Button";
 import { Modal } from "../components/Modal/Modal";
-import { alert, error } from "@pnotify/core";
+// import { error } from "@pnotify/core";
 import "@pnotify/core/dist/PNotify.css";
 import "@pnotify/core/dist/BrightTheme.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "../components/Loader/Loader";
 
 function App() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
-  const [bigImg, setBigImg] = useState('');
+  const [bigImg, setBigImg] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -23,153 +23,66 @@ function App() {
     setValue(value);
     setPage(1);
     setImages([]);
+
+    setIsLoading(true);
+    pixabayFetch(value, page)
+      .then((images) => {
+        setImages([...images]);
+      })
+      .catch((error) =>
+        error("There are no images!!!")
+      )
+      .finally(() => setIsLoading(false));
   };
-  useEffect(() => {
-    if(value === ''){return}
-    fetchImages()
-  }, [value, page]);
+
+  const fetchImages = () => {
+    setIsLoading(!isLoading);
+
+    pixabayFetch(value, page)
+      .then((images) => {
+        setImages((state) => [...state, ...images]);
+        setPage((prevState) => prevState + 1);
+      })
+      .catch((error) =>
+        error({
+          text: "No image!",
+          delay: 1000,
+        })
+      )
+      .finally(() => setIsLoading(!isLoading));
+  };
 
   useEffect(() => {
-    if (!modalIsOpen) {
+    if (images) {
       window.scrollTo({
         top: document.documentElement.scrollHeight,
         behavior: "smooth",
       });
     }
-  }, [modalIsOpen]);
-
-  const fetchImages = () => {  
-    const options = {
-      value,
-       page ,
-    };
-    
-  setIsLoading(!isLoading);
-    pixabayFetch(options)
-      .then((images) => {
-      setImages((prevState) => {
-          return [...prevState, ...images]           
-          
-      });
-        setPage(state=> state +1)
-      }).catch((error) =>
-        error({
-          text: "No image!",
-          delay: 1000,
-        })      
-      ).finally(() => setIsLoading(!isLoading));      
-};
+  }, [images]);
   
   const imgBig = (img) => {
     setBigImg(img);
     toggleModal();
   };
-
+  
   const toggleModal = () => {
     setModalIsOpen(!modalIsOpen);
   };
 
   return (
-      <div className="Container">
-        <Searchbar onSubmit={formSubmitHandle} />
-        {isLoading && <Loader />}
-        <ImageGallery images={images} onClick={imgBig} />
-        {images.length > 0 && (
-          <Button text={"load more"} onClick={fetchImages} />
-        )}
-        {modalIsOpen && (
-          <Modal showModal={imgBig}>
-            <img src={bigImg} alt="" />
-          </Modal>
-        )}
-      </div>
-    );
-  }
-
-
-
-
-
-
-
-// class App extends React.Component {
-//   state = {
-//     value: "",
-//     page: 1,
-//     images: [],
-//     isLoading: false,
-//     modalIsOpen: false,
-//     bigImg: "",
-//   };
-//   formSubmitHandle = (value) => {
-//     this.setState({
-//       value: value,
-//       page: 1,
-//       images: [],
-//     });
-//   };
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevState.value !== this.state.value) {
-      
-//       this.fetchImages();
-//     }
-//      if (!this.state.modalIsOpen) {
-//       window.scrollTo({
-//       top: document.documentElement.scrollHeight,
-//       behavior: "smooth",
-//     });
-//     }
-//   }
-//   fetchImages = () => {
-//     const options = {
-//       query: this.state.value,
-//       page: this.state.page,
-//     };
-//     this.setState({ isLoading: true });
-//     pixabayFetch(options)
-//       .then((images) => {
-//         this.setState((prevState) => {
-//           return {
-//             images: [...prevState.images, ...images],
-//             page: prevState.page + 1,
-//           };
-//         });
-//       }).catch((error) =>
-//         error({
-//           text: "No image!",
-//           delay: 1000,
-//         })      
-//       ).finally(() => this.setState({ isLoading: false }));      
-//   };
-//   imgBig = (img) => {
-   
-//     this.setState({ bigImg: img });
-
-//     this.toggleModal();
-//   };
-//  toggleModal = () => {
-//     this.setState(state => ({
-//       modalIsOpen: !state.modalIsOpen,
-//     }));
-//   };
-//   render() { 
-//     const {images, bigImg, isLoading, modalIsOpen} = this.state
-//     return (
-//       <div className="Container">
-//         <Searchbar onSubmit={this.formSubmitHandle} />
-//         {isLoading && <Loader />}
-//         <ImageGallery images={images} onClick={this.imgBig} />
-//         {images.length > 0 && (
-//           <Button text={"load more"} onClick={this.fetchImages} />
-//         )}
-//         {modalIsOpen && (
-//           <Modal showModal={this.imgBig}>
-//             <img src={bigImg} alt="" />
-//           </Modal>
-//         )}
-//       </div>
-//     );
-//   }
-// }
+    <div className="Container">
+      <Searchbar onSubmit={formSubmitHandle} />
+      {isLoading && <Loader />}
+      <ImageGallery images={images} onClick={imgBig} />
+      {images.length > 0 && <Button text={"load more"} onClick={fetchImages} />}
+      {modalIsOpen && (
+        <Modal showModal={imgBig} modalState={modalIsOpen}>
+          <img src={bigImg} alt="" />
+        </Modal>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,66 +1,28 @@
-// import PropTypes from 'prop-types';
-// import React from 'react';
-
-
-// export class Modal extends React.Component{
- 
-//  componentDidMount() {
-//     window.addEventListener('keydown', this.modalClose);
-//   };
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.modalClose);
-//   };
- 
-//   modalClose = (e) => {
-//     if (e.currentTarget === e.target || e.code === 'Escape') {   
-//       this.setState({ modalIsOpen: false });
-//     }
-//   };
-  
-//   render() {
-//     return (
-//         <div className="Overlay" onClick = {this.modalClose}>
-//   <div className="Modal">
-//     <img src={this.props.img} alt="" />
-//   </div>
-// </div>
-//     )
-//   }
-// }
-
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const portal = document.querySelector('#portal');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.showModal();
-    }
-  };
-
-  handleOverlayClick = event => {
+export function Modal({ showModal, children }) {
+  const handleOverlayClick = (event) => {
     if (event.currentTarget === event.target) {
-    this.props.showModal();
+      showModal();
+    };
+  };
+  const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+      showModal();
+      window.removeEventListener('keydown', handleKeyDown);
     }
   };
- 
-  render() {
-    return createPortal(
-      <div className='Overlay' onClick={this.handleOverlayClick}>
-        <div className='Modal'>{this.props.children}</div>
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+   return createPortal(
+      <div className='Overlay' onClick={handleOverlayClick}>
+        <div className='Modal'>{children}</div>
       </div>,
       portal,
     );
-  }
-}
+};
